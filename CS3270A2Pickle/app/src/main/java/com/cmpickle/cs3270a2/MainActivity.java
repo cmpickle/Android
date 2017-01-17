8b9166fbfc1a3e9c4d08edc5af44cd0448f32e13
Cameron Pickle <cmpickle@gmail.com>
2017-01-16 19:08:48 -0700

    fragment containers and fragments added

--- CS3270A2Pickle/app/src/main/java/com/cmpickle/cs3270a2/MainActivity.java
+++ CS3270A2Pickle/app/src/main/java/com/cmpickle/cs3270a2/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction().add(R.id.fragmentContainer1, new FragmentA(), "FA").commit();
     }
 }
