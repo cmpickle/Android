57aafd2e1e63f09961f203f927f336622456faa3
Cameron Pickle <cmpickle@gmail.com>
2017-02-09 20:17:11 -0700

    added main fragments, fragment layouts, and menu for A5

--- CS3270A5Pickle/app/src/main/java/com/cmpickle/cs3270a5/MainActivity.java
+++ CS3270A5Pickle/app/src/main/java/com/cmpickle/cs3270a5/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cmpickle.cs3270a5;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.changeResultsContainer, new ChangeResultsFragment(), "changeResultsFragment");
+        fragmentTransaction.replace(R.id.changeButtonsContainer, new ChangeButtonsFragment(), "changeButtonsFragment");
+        fragmentTransaction.replace(R.id.changeActionsContainer, new ChangeActionsFragment(), "changeActionsFragment");
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
